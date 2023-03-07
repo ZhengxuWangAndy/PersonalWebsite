2f2170cb1b85d681280f64d9c9b6de4791a72524
Zhengxu Wang <zhengxuwang519@gmail.com>
2023-03-06 22:09:21 -0500

    photo updated

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.JPG";
+import myImg from "../../Assets/avatar.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
