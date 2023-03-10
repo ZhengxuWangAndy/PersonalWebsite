de1dd8ae665806ebb6146ace13bd58d892a7a6bc
Zhengxu Wang <zhengxuwang519@gmail.com>
2023-03-10 01:49:33 -0500

    update projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,7 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import UmpScores from "../../Assets/Projects/UmpScores.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import Atos from "../../Assets/Projects/Atos.png";
+import research from "../../Assets/Projects/research.jpg";
 
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
@@ -26,10 +27,10 @@ function Projects() {
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Row style={{ justifyContent: "center", paddingBottom: "1px" }}>
 
           {/* project: BU Spark & UmpScores */}
-          <Col md={4} className="project-card">
+          <Col xs={12} sm={6} md={4} className="project-card">
             <Card className="project-card-view">
               <Card.Img variant="top" src={UmpScores} alt="card-img" />
               <Card.Body>
@@ -37,6 +38,8 @@ function Projects() {
                   <br />
                   <strong style={{ color: "limegreen" }}>{"BU Spark & UmpScores"}</strong></Card.Title>
                 <Card.Text style={{ textAlign: "justify" }}>
+                  {"Provided accurate and transparent season long metrics and individual game performance measurements of baseball games."}
+                  <br />
                   <strong style={{ color: "white" }}>
                     {"Front-end: "}</strong>
                   <br />
@@ -69,68 +72,130 @@ function Projects() {
           </Col>
 
           {/* project: Atos work */}
-          <Col md={4} className="project-card">
+          <Col xs={12} sm={6} md={4} className="project-card">
             <Card className="project-card-view">
-              <Card.Img variant="top" src={Atos} alt="card-img" />
+              <Card.Img variant="top" src={Atos} alt="card-img" style={{ height: "250px" }} />
               <Card.Body>
                 <Card.Title><strong style={{ color: "white" }}>{"Data Engineer"} </strong>
                   <br />
                   <strong style={{ color: "limegreen" }}>{"Atos Technology Co. Ltd"}</strong></Card.Title>
                 <Card.Text style={{ textAlign: "justify" }}>
                   <strong style={{ color: "white" }}>
-                    {"Front-end: "}</strong>
-                  <br />
-                  <strong style={{ color: "limegreen" }}>
-                    {"React-redux"}</strong> {" ("} <strong style={{ color: "limegreen" }}>{"JavaScript"} </strong>{" Framework). "}
-                  {"Create a new table get backend"} <strong style={{ color: "limegreen" }}>{"RestfulAPI"}</strong>{" data. "}
-
-                  <br />
-                  <strong style={{ color: "white" }}>
                     {"Back-end: "}</strong>
                   <br />
-                  <strong style={{ color: "limegreen" }}>
-                    {"Flask (Python) "}</strong>{"with data stored in "}<strong style={{ color: "limegreen" }}>{"AWS. "}</strong>
-
-                  {"Update the back-end using newer libraries make the app more scalability."}
+                  <strong style={{ color: "limegreen" }}>{"Springboot Admin"}</strong>{" starts the "} <strong style={{ color: "limegreen" }}> {"Neo4j and MySql"}</strong> {" databases, "}<strong style={{ color: "limegreen" }}>{"Nacos starts the Java-written RestfulAPI"}</strong>{" processor and data synchronization program, use "}<strong style={{ color: "limegreen" }}>{"docker"}</strong>{" to deploy."}
+                  <br />
+                  <strong style={{ color: "white" }}>
+                    {"Frontend: "}</strong>
+                  <br />
+                  <strong style={{ color: "limegreen" }}>{"VUE.js and AntV"}</strong>{" implement multi-level click expansion of nodes in the graph and file jumping."}
+                  <br />
+                  <strong style={{ color: "white" }}>{"Search Engine:"}</strong>
+                  <br />
+                  {"Optimized the "}<strong style={{ color: "limegreen" }}>{"ElasticSearch micro-service"}</strong>{", delivering KG-based search and recommendation functionality and implementing a scalable, distributed deployment. Connected the system to "}<strong style={{ color: "limegreen" }}>{"OracleDB"}</strong>{" to provide efficient data management and use "}<strong style={{ color: "limegreen" }}>{"Redis"}</strong>{" to accelerate high concurrency search speed."}
                 </Card.Text>
-
-                {"\n"}
-                {"\n"}
                 {
                   <Button
                     variant="primary"
-                    onClick={() => window.open(Atos)}
+                    onClick={() => window.open("https://docs.google.com/document/d/13vf_hrTjylKOiNNaSsSu66Qfb9WAUkr4M1Uwl7ktNyg/edit?usp=sharing")}
                     style={{ marginLeft: "10px" }}
                   >
                     <CgWebsite /> &nbsp;
-                    {"Open Image"}
+                    {"Demo document"}
                   </Button>
                 }
               </Card.Body>
             </Card>
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
-            />
+          {/* research project */}
+          <Col xs={12} sm={6} md={4} className="project-card">
+            <Card className="project-card-view">
+              <Card.Img variant="top" src={research} alt="card-img" style={{ height: "250px" }} />
+              <Card.Body>
+                <Card.Title><strong style={{ color: "white" }}>{"Research Assistant"} </strong>
+                  <br />
+                  <strong style={{ color: "limegreen" }}>{"Chinese Academy of Sciences"}</strong></Card.Title>
+                <Card.Text style={{ textAlign: "justify" }}>
+                  <br />
+                  {"MRI Brain Image Segmentation Assist Alzheimer’s Diagnosis"}
+                  <br />
+                  <br />
+                  {"Added "}<strong style={{ color: "limegreen" }}>{"Self-Attention and Generalized-Dice Loss to PyTorch-based 3D-Unet model"}</strong>{" to segment the Hippocampus region and automatically quantify the degree of Hippocampal atrophy. The dice coefficient of the segmentation result exceeds 0.9."}
+                  <br />
+                  <br />
+                  {"Completed the visualization of model prediction results using "}<strong style={{ color: "limegreen" }}>{"Matplotlib and FreeSurfer."}</strong>
+
+
+                </Card.Text>
+                {
+                  <Button
+                    variant="primary"
+                    onClick={() => window.open("https://drive.google.com/file/d/1WVqT6weYu9A2P0LqteIFvl5sVO8RiIn8/view")}
+                    style={{ marginLeft: "10px" }}
+                  >
+                    <CgWebsite /> &nbsp;
+                    {"PDF"}
+                  </Button>
+                }
+                {
+                  <Button
+                    variant="primary"
+                    onClick={() => window.open("https://kns.cnki.net/kcms2/article/abstract?v=3uoqIhG8C44YLTlOAiTRKibYlV5Vjs7iJTKGjg9uTdeTsOI_ra5_XaTH7EbalgZBjFBbsEwRY2Nvn0CrlW-IRHBRCWY_UhRB&uniplatform=NZKPT")}
+                    style={{ marginLeft: "10px" }}
+                  >
+                    <CgWebsite /> &nbsp;
+                    {"CNKI"}
+                  </Button>
+                }
+              </Card.Body>
+            </Card>
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
+          {/* research project */}
+          <Col xs={12} sm={6} md={4} className="project-card">
+            <Card className="project-card-view">
+              <Card.Img variant="top" src={research} alt="card-img" style={{ height: "250px" }} />
+              <Card.Body>
+                <Card.Title><strong style={{ color: "white" }}>{"Research Assistant"} </strong>
+                  <br />
+                  <strong style={{ color: "limegreen" }}>{"Chinese Academy of Sciences"}</strong></Card.Title>
+                <Card.Text style={{ textAlign: "justify" }}>
+                  <br />
+                  {"MRI Brain Image Segmentation Assist Alzheimer’s Diagnosis"}
+                  <br />
+                  <br />
+                  {"Added "}<strong style={{ color: "limegreen" }}>{"Self-Attention and Generalized-Dice Loss to PyTorch-based 3D-Unet model"}</strong>{" to segment the Hippocampus region and automatically quantify the degree of Hippocampal atrophy. The dice coefficient of the segmentation result exceeds 0.9."}
+                  <br />
+                  <br />
+                  {"Completed the visualization of model prediction results using "}<strong style={{ color: "limegreen" }}>{"Matplotlib and FreeSurfer."}</strong>
+
+
+                </Card.Text>
+                {
+                  <Button
+                    variant="primary"
+                    onClick={() => window.open("https://drive.google.com/file/d/1WVqT6weYu9A2P0LqteIFvl5sVO8RiIn8/view")}
+                    style={{ marginLeft: "10px" }}
+                  >
+                    <CgWebsite /> &nbsp;
+                    {"PDF"}
+                  </Button>
+                }
+                {
+                  <Button
+                    variant="primary"
+                    onClick={() => window.open("https://kns.cnki.net/kcms2/article/abstract?v=3uoqIhG8C44YLTlOAiTRKibYlV5Vjs7iJTKGjg9uTdeTsOI_ra5_XaTH7EbalgZBjFBbsEwRY2Nvn0CrlW-IRHBRCWY_UhRB&uniplatform=NZKPT")}
+                    style={{ marginLeft: "10px" }}
+                  >
+                    <CgWebsite /> &nbsp;
+                    {"CNKI"}
+                  </Button>
+                }
+              </Card.Body>
+            </Card>
           </Col>
+
 
           <Col md={4} className="project-card">
             <ProjectCard
